0e9e3d5e1666d9fac120a09672d6184be0fcc08e
HJ S <hjseong1222@gmail.com>
2023-07-11 11:53:15 +0900

    0711 jest - 비동기2

--- js_jest/asyncfn.js
+++ js_jest/asyncfn.js
@@ -15,11 +15,34 @@ function fetchUser(id, checkTest) {
 }
 
 // 비동기함수
+// Promise 를 이용하여서 비동기 함수 작성
+// return 값으로 Promise 객체를 주게되면, 
+// then, await를 통해서 값을 가져올수 있다
+// ex) fetch API, axios
+function fetchUserPromise(id) {
+    // Promise객체 생성
+    // : 시간이 걸리는 함수를 넣어줌
+    // Promise 안에 함수의 매개변수로 resolve 함수를 사용하여 return값 전달
+    return new Promise((resolve)=>{
+        // 시간이 걸리는 내용 작성
+        console.log("0.1가 걸립니다")
+
+        // setTimeout 을 이용하여 시간이 걸리는 내용 작성
+        setTimeout(() => {
+            const user = {
+                id : id,
+                name : "User"+id
+            }
+            resolve(user);
+        }, 100);
+    })
+}
 
 
 // 함수를 전달할 객체
 const asyncfn = {
-    fetchUser
+    fetchUser,
+    fetchUserPromise
 }
 
 // 내보내서 사용
--- js_jest/asyncfn.test.js
+++ js_jest/asyncfn.test.js
@@ -13,3 +13,22 @@ test('setTimeout 확인', (done) => {
         done();
     })
 })
+
+// Promise 값을 전달하는 함수 실행 
+// 1. then
+// 값을 가져올때 return 을 붙여서 사용
+test('user Promise', () => { 
+    return asyncfn.fetchUserPromise("2").then(
+        //then을 통해서 함수를 이용하여 값을 가져올수 있다
+        //user는 resolve 통해서 가져온 값
+        (user) => { 
+            // then 안에서 가져온 값 확인
+            expect(user).toEqual({id:"2",name:"User2"})
+        });
+ })
+
+//2. async/await
+test('user Promise await', async () => { 
+    const user = await asyncfn.fetchUserPromise("3");
+    expect(user).toEqual({id:"3", name:"User3"})
+ })
