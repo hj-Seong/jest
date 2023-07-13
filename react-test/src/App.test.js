1a842bdb2ec71224d915a5ece7a407f99c35e9da
HJ S <hjseong1222@gmail.com>
2023-07-13 11:45:29 +0900

    0713 jest - App

--- react-test/src/App.test.js
+++ react-test/src/App.test.js
@@ -2,11 +2,30 @@
 // screen : 렌더이후에 화면에출력되는 html 엘리멘트를 가져오는 객체
 // >> render를 통해서 가져와 쓸수 도 있다 
 //  ex) const {getByText} = render(컴포넌트)
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// count=0  인지 확인
+test('App에서 CountView가 count:0 확인', () => {
   render(<App />);
-  //const linkElement = screen.getByText(/리액트 공부/i);
-  //expect(linkElement).toBeInTheDocument();
+  // Text를 통해서 값이 제대로 들어갔는지 확인
+  const view = screen.getByText('현재숫자 :0');
+  expect(view).toBeInTheDocument();
 });
+
+// 증가버튼을 눌렀을때 값이 증가하는지
+it('App에서 증가버튼을 눌렀을때 값이 바뀌는지 ', () => {
+  render(<App />)
+  // 증가버튼
+  const incrementBtn = screen.getByTestId('incrementBtn');
+
+  // 이벤트 실행후 증가값확인
+  fireEvent.click(incrementBtn);
+  fireEvent.click(incrementBtn);
+
+  const changeState = screen.getByText('현재숫자 :2');
+  expect(changeState).toBeInTheDocument();
+});
+
+// 감소버튼을 눌렀을때 값이 감소하는지
+// 직접연습
